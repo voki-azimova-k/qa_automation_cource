f6956340ab4d87cbe2dd75ebbfce780ba15aa0af
voki-azimova-k <kateryna.azimova@vokigames.com>
2026-08-04 16:22:37 +0300

    Implement basic calculator operations

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def add(first, second):
+    return first + second
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def subtract(first, second):
+    return first - second
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def multiply(first, second):
+    return first * second
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def divide(first, second):
+    return first / second
+
+
+print(add(2, 3))
+print(subtract(2, 3))
+print(multiply(2, 3))
+print(divide(2, 3))
